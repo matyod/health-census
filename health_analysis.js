27fa8de89bbf55d6aedc87101e106bf727e5343a
matyod <109255635+matyod@users.noreply.github.com>
2025-04-26 00:42:31 +0800

    Add addPatient() method

--- health_analysis.js
+++ health_analysis.js
@@ -1,7 +1,52 @@
 const btnSearch = document.getElementById('btnSearch');
 const addPatientButton = document.getElementById('addPatient');
+const errorMsg = document.getElementById('errorMsg');
+errorMsg.style.color = 'red';
 const report = document.getElementById('report');
+
+const nameElement = document.getElementById('name');
+const ageElement = document.getElementById('age');
+const conditionElement = document.getElementById('condition');
 
 // Store collected patients data
 const patients = [];
 
+function addPatient() {
+  const name = nameElement.value.trim();
+
+  const selectedGender = document.querySelector('input[name="gender"]:checked');
+  const gender = selectedGender ? selectedGender.value.trim() : '';
+
+  const age = parseInt(ageElement.value.trim());
+  const condition = conditionElement.value.trim();
+
+  if (name && gender && age && condition) {
+    errorMsg.textContent = '';
+
+    patients.push({
+      name: name,
+      gender: gender,
+      age: age,
+      condition: condition,
+    });
+
+    // // Log patients data
+    // let index = 0;
+    // patients.forEach(patient => {
+    //   // Object.entries(patient).forEach(([key, value]) => {
+    //   //   console.log(`${key}: ${value}`);
+    //   // });
+
+    //   console.log(`${++index}:`);
+    //   for (const key in patient) {
+    //     console.log(`${key}: ${patient[key]}`);
+    //   }
+    // });
+    // console.log('━━━━━━━━━━━━━━━');
+  } else {
+    errorMsg.textContent = "All fields need to be filled in/selected";
+    return;
+  }
+}
+
+addPatientButton.addEventListener('click', addPatient);
