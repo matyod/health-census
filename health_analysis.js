8c7c335bb9e95e5ac194916103bd9f772fd776c1
matyod <109255635+matyod@users.noreply.github.com>
2025-04-25 16:14:02 +0800

    Initial commit

---
+++ health_analysis.js
@@ -0,0 +1,7 @@
+const btnSearch = document.getElementById('btnSearch');
+const addPatientButton = document.getElementById('addPatient');
+const report = document.getElementById('report');
+
+// Store collected patients data
+const patients = [];
+
